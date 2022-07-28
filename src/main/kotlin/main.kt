26c8346c34b3073f5556338d0979feebf7882f95
AntonKuznetsov2022 <anton_kuznetsov@bk.ru>
2022-07-28 20:02:29 +0300

    second commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,10 @@
 fun main () {
+    val likes: Int = 62
+    if (likes%10 == 1) println("Понравилось $likes человеку") else println("Понравилось $likes человекам")
 
+    /*
+    в соответствии с правлами русского языка с числительными используется только слово человек-
+    http://new.gramota.ru/spravka/buro/search-answer?s=человеками
+    https://rg.ru/2016/02/11/kak-govorit-pravilno-chelovek-ili-liudi.html
+    */
 }
