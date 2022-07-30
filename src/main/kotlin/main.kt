be1edd0c19f4ba95def3c3dd128a841443f64bbe
AntonKuznetsov2022 <anton_kuznetsov@bk.ru>
2022-07-30 19:50:21 +0300

    One more commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,4 @@
 fun main () {
-    val likes: Int = 62
-    if (likes%10 == 1) println("Понравилось $likes человеку") else println("Понравилось $likes человекам")
-
-    /*
-    в соответствии с правлами русского языка с числительными используется только слово человек-
-    http://new.gramota.ru/spravka/buro/search-answer?s=человеками
-    https://rg.ru/2016/02/11/kak-govorit-pravilno-chelovek-ili-liudi.html
-    */
+    val likes: Int = 21
+    if (likes%10 == 1 && likes%100 != 11) println("Понравилось $likes человеку") else println("Понравилось $likes человекам")
 }
