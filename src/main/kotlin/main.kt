38f2cba664ceae8d7501746cf297f40b32796ced
AntonKuznetsov2022 <anton_kuznetsov@bk.ru>
2022-08-01 21:37:23 +0300

    commit 01.08

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,7 @@
 fun main () {
-    val likes: Int = 21
-    if (likes%10 == 1 && likes%100 != 11) println("Понравилось $likes человеку") else println("Понравилось $likes человекам")
+    val likes: Int = 11
+    var namePeople = ""
+    if (likes%10 == 1 && likes%100 != 11)  namePeople = "человеку" else namePeople = "человекам"
+
+    println("Понравилось $likes $namePeople")
 }
